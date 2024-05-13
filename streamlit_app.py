aa99943eff9b320bd72248c61d38f2b6db2dfebb
Sveinung Christophersen <133006393+sveinungchr@users.noreply.github.com>
2024-05-13 13:24:20 +0200

    1234

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if Ingredients_list:
 
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + SEARCH_ON)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
